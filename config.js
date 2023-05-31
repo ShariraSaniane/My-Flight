da1b2e9b5733daddb7ca248472eff7125b9d263d
Salsabila <brilianabila@gmail.com>
2023-06-01 00:02:25 +0700

    renew

--- config.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebase/serviceAccount.json');
-
-const firebase = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = firebase
--- models/users.js
+++
@@ -1,6 +0,0 @@
-const { defineModel, DataTypes } = require("firestore-sequelize");
-exports.User = defineModel("users", {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-});
